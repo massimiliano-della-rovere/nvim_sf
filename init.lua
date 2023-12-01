b9acc499bf583fb00915d617de492f7291695921
Massimiliano della Rovere <massimiliano.dellarovere@gmail.com>
2023-12-01 16:28:46 +0100

    added pomo(doro).nvim plugin

--- init.lua
+++ init.lua
@@ -73,6 +73,21 @@ require('lazy').setup({
   'nvim-treesitter/nvim-treesitter',
   'nvim-treesitter/nvim-treesitter-textobjects',
 
+  -- pomodoro timer
+  {
+    "epwalsh/pomo.nvim",
+    version = "*",  -- Recommended, use latest release instead of latest commit
+    lazy = true,
+    cmd = { "TimerStart", "TimerStop", "TimerRepeat" },
+    dependencies = {
+      -- Optional, but highly recommended if you want to use the "Default" timer
+      "rcarriga/nvim-notify",
+    },
+    opts = {
+      -- See below for full list of options 👇
+    },
+  },
+
   -- :cmd popup
   {
     "folke/noice.nvim",
